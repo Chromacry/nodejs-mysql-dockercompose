741bb86df114f09ffd1e6742d8cd31d7fc39458e
Chromacry <Chromacry101@gmail.com>
2022-10-15 22:11:14 +0800

    Added volume for DB and timezone fix

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,6 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
-ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
+-- Uncomment below for changing account credentials, docker for first init only
+-- ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
+-- ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
+
+SET GLOBAL time_zone = '+00:00';
+SET @@session.time_zone = "+00:00";
